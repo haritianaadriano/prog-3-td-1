95156fdc2eff5f3b0e7e972bb377a6f7d5275fff
haritiana adriano <hei.adriano.4@gmail.com>
2022-12-12 11:23:31 +0300

    feat: author repository

--- src/main/java/app/prog/model/AuthorEntity.java
+++ src/main/java/app/prog/model/AuthorEntity.java
@@ -1,4 +1,28 @@
 package app.prog.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "author")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class AuthorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private LocalDate birthDate;
+    private String particularity;
+
+    public boolean hasParticularity(){
+        return particularity != null;
+    }
 }
